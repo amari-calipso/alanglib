81b0173fe9ea3e0c4f068ca6941e957dd4a7c37c
Amari <68780017+amari-calipso@users.noreply.github.com>
2025-09-10 00:14:28 +0200

    add str variants for char checks

--- src/scanner.rs
+++ src/scanner.rs
@@ -31,3 +31,35 @@ pub fn is_alphanumeric(c: char) -> bool {
 pub fn substring(string: &String, a: usize, b: usize) -> String {
     string.chars().skip(a).take(b - a).collect()
 }
+
+fn char_check_str(c: &str, function: fn(char) -> bool) -> bool {
+   c.chars().nth(0).map(|x| function(x)).unwrap_or(false)
+}
+
+pub fn is_str_beginning_digit(c: &str) -> bool {
+    char_check_str(c, is_beginning_digit)
+}
+
+pub fn is_str_digit(c: &str) -> bool {
+    char_check_str(c, is_digit)
+}
+
+pub fn is_str_bin_digit(c: &str) -> bool {
+    char_check_str(c, is_bin_digit)
+}
+
+pub fn is_str_oct_digit(c: &str) -> bool {
+    char_check_str(c, is_oct_digit)
+}
+
+pub fn is_str_hex_digit(c: &str) -> bool {
+    char_check_str(c, is_hex_digit)
+}
+
+pub fn is_str_alpha(c: &str) -> bool {
+    char_check_str(c, is_alpha)
+}
+
+pub fn is_str_alphanumeric(c: &str) -> bool {
+    char_check_str(c, is_alphanumeric)
+}
